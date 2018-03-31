a7355185b7dd185f2b00154aec1374104e41a8c5
Hasnaa <hasnaamohammed1993@gmail.com>
2018-03-31 18:45:17 +0200

    the Notification

--- app/src/main/java/com/app/egh/tripplanner/activities/AlarmActivity.java
+++ app/src/main/java/com/app/egh/tripplanner/activities/AlarmActivity.java
@@ -1,7 +1,15 @@
 package com.app.egh.tripplanner.activities;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,7 +39,7 @@ public class AlarmActivity extends AppCompatActivity {
        setContentView(R.layout.activity_alarm);
 
        Intent intent = getIntent();
-       Trip currentTrip = (Trip) intent.getSerializableExtra("tripReminder");
+       final Trip currentTrip = (Trip) intent.getSerializableExtra("tripReminder");
 
         new MaterialStyledDialog.Builder(this)
                 .setTitle(currentTrip.getTrip_name())
@@ -42,7 +50,7 @@ public class AlarmActivity extends AppCompatActivity {
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         finish();
                         Log.i("MaterialStyledDialogs", "Do something!");
-                        Toast.makeText(getApplicationContext(),"start trip",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"start trip" + currentTrip.getTrip_id(),Toast.LENGTH_SHORT).show();
 
                     }})
                 .setStyle(Style.HEADER_WITH_TITLE)
@@ -64,7 +72,53 @@ public class AlarmActivity extends AppCompatActivity {
                         Log.i("MaterialStyledDialogs", "Do something!");
                         Toast.makeText(getApplicationContext(),"postpone trip",Toast.LENGTH_SHORT).show();
 
+                        showNotification(AlarmActivity.this,currentTrip.getTrip_id(), currentTrip.getTrip_name(),"Want to start the Trip?", currentTrip);
+
+
                     }})
                 .show();
     }
+    private void showNotification(Context mContext, int notificationId, String title, String content, Trip trip) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(mContext.getApplicationContext(), "notify_001");
+
+        Intent intent = new Intent(mContext, AlarmActivity.class);
+        if (trip != null) {
+            intent.putExtra("tripReminder", trip);
+        }
+        PendingIntent pendingIntent = PendingIntent.getActivity(mContext, notificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(pendingIntent)
+                // .setSmallIcon(R.drawable.app_logo_notification)
+                .setSmallIcon(R.drawable.places_ic_search)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(true)
+                .setOngoing(true)
+
+                //  .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.app_logo));
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.places_ic_search));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mBuilder.setPriority(Notification.PRIORITY_MAX);
+        }
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notify_001",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            if (mNotificationManager != null) {
+                mNotificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        if (mNotificationManager != null) {
+            mNotificationManager.notify(notificationId, mBuilder.build());
+        }
+    }
+
 }
