011af551f238473e8b632aeaf47083cb431e0229
Hasnaa <hasnaamohammed1993@gmail.com>
2018-04-01 11:16:00 +0200

    Reminder ringtone

--- app/src/main/java/com/app/egh/tripplanner/activities/AlarmActivity.java
+++ app/src/main/java/com/app/egh/tripplanner/activities/AlarmActivity.java
@@ -7,6 +7,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -30,6 +32,7 @@ import java.util.Locale;
 
 public class AlarmActivity extends AppCompatActivity {
     final static String TAG = "powerLock";
+    Ringtone r;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +47,22 @@ public class AlarmActivity extends AppCompatActivity {
        setContentView(R.layout.activity_alarm);
 
        Intent intent = getIntent();
+        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        if (alert == null) {
+            // alert is null, using backup
+            alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+            // I can't see this ever being null (as always have a default notification)
+            // but just incase
+            if (alert == null) {
+                // alert backup is null, using 2nd backup
+                alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+
+         r = RingtoneManager.getRingtone(getApplicationContext(), alert);
+        r.play();
        final Trip currentTrip = (Trip) intent.getSerializableExtra("tripReminder");
 
         new MaterialStyledDialog.Builder(this)
@@ -54,6 +73,7 @@ public class AlarmActivity extends AppCompatActivity {
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         finish();
+                        r.stop();
                         Log.i("MaterialStyledDialogs", "Do something!");
                         Toast.makeText(getApplicationContext(),"start trip" + currentTrip.getTrip_id(),Toast.LENGTH_SHORT).show();
 
@@ -102,6 +122,7 @@ public class AlarmActivity extends AppCompatActivity {
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         finish();
+                        r.stop();
                         Log.i("MaterialStyledDialogs", "Do something!");
                         Toast.makeText(getApplicationContext(),"cancel",Toast.LENGTH_SHORT).show();
 
@@ -111,6 +132,7 @@ public class AlarmActivity extends AppCompatActivity {
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         finish();
+                        r.stop();
                         Log.i("MaterialStyledDialogs", "Do something!");
                         Toast.makeText(getApplicationContext(),"postpone trip",Toast.LENGTH_SHORT).show();
 
@@ -162,5 +184,59 @@ public class AlarmActivity extends AppCompatActivity {
             mNotificationManager.notify(notificationId, mBuilder.build());
         }
     }
+/* new MaterialStyledDialog.Builder(this)
+         .setTitle(currentTrip.getTrip_name())
+            .setDescription("Do you want to start your trip now?")
+                .setPositiveText("Ok")
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+        @Override
+        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+            finish();
+            Log.i("MaterialStyledDialogs", "Do something!");
+            Toast.makeText(getApplicationContext(),"start trip" + currentTrip.getTrip_id(),Toast.LENGTH_SHORT).show();
 
+            Trip tripData = currentTrip;
+            //int itemPosition = recyclerView.getChildLayoutPosition(v);
+            //Trip tripData = tripDataList.get(itemPosition);
+            // Trip tripData = tripDataList.get(0);
+            tripData.setStarted(true);
+            String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f(%s)&daddr=%f,%f (%s)", tripData.getStart_lat(), tripData.getStart_long(), tripData.getStart_name(),  tripData.getEnd_lat() , tripData.getEnd_long(), tripData.getEnd_name());
+            if(tripData.isRoundtrip()){
+                tripData.setStarted(false);
+                tripData.setRoundtrip(false);
+                // add 2 hours
+                final long millisToAdd = 7_200_000; //two hours
+                Date d = tripData.getDate_time();
+                d.setTime(d.getTime() + millisToAdd);
+                tripData.setDate_time(d);
+                // swip lat long
+                double temp_lat = tripData.getStart_lat();
+                double temp_long = tripData.getStart_long();
+                String temp_name = tripData.getStart_name();
+
+                tripData.setStart_name(tripData.getEnd_name());
+                tripData.setStart_lat(tripData.getEnd_lat());
+                tripData.setStart_long(tripData.getEnd_long());
+
+                tripData.setEnd_name(temp_name);
+                tripData.setEnd_lat(temp_lat);
+                tripData.setEnd_long(temp_long);
+
+            }
+            Adapter dbAdapter = new Adapter(AlarmActivity.this);
+
+            dbAdapter.updateTrip(tripData);
+
+            //Toast.makeText( context, "Start trip activity", Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+            intent.setPackage("com.google.android.apps.maps");
+            startActivity(intent);
+        }})
+            .setStyle(Style.HEADER_WITH_TITLE)
+                .setHeaderColor(R.color.colorAccent)
+
+
+            .show();
+            */
 }
