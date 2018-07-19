658f22390799f9047a2906a0f25a93e6cee8741b
Hasnaa <hasnaamohammed1993@gmail.com>
2018-07-20 01:14:50 +0200

    another commit

--- app/src/main/java/com/app/egh/tripplanner/activities/AddTripActivity.java
+++ app/src/main/java/com/app/egh/tripplanner/activities/AddTripActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import com.app.egh.tripplanner.R;
 
 public class AddTripActivity extends AppCompatActivity {
-
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
